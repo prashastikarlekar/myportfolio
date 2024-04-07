999ed41cfe76ca15ff02ff95fb8bb4b222c767d9
Prashasti Karlekar <prashastikarlekar@gmail.com>
2024-04-06 21:17:10 -0500

    bug fixes

--- components/blocks/projects/featured.jsx
+++ components/blocks/projects/featured.jsx
@@ -1,112 +1,147 @@
-import Image from 'next/image'
+/** @format */
 
-import { useEffect } from 'react'
-import { m, useAnimation } from "framer-motion"
-import { useInView } from 'react-intersection-observer'
+import Image from "next/image";
 
-import Badges 		from '../../utils/badge.list.util'
-import Icon 		from '../../utils/icon.util'
+import { useEffect } from "react";
+import { m, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-import css 			from '../../../styles/sections/projects/featured.module.scss'
-import content 		from '../../../content/projects/featured.json'
+import Badges from "../../utils/badge.list.util";
+import Icon from "../../utils/icon.util";
+
+import css from "../../../styles/sections/projects/featured.module.scss";
+import content from "../../../content/projects/featured.json";
+import button from "../../../styles/blocks/button.module.scss";
 
 export default function FeaturedProject({ content }, index) {
-
-	const { project, url, repo, descriptionTitle,description, stack, imageOptions, images } = content
+	const {
+		project,
+		url,
+		repo,
+		descriptionTitle,
+		features,
+		stack,
+		imageOptions,
+		images,
+	} = content;
 
 	const controls = useAnimation();
-	const { ref, inView  } = useInView({
-		"threshold": 0.25,
-		"triggerOnce": false
-	})
+	const { ref, inView } = useInView({
+		threshold: 0.25,
+		triggerOnce: false,
+	});
 
-	useEffect( () => {
-		if ( inView ) {	controls.start("visible") }
-		if ( !inView ) { controls.start("hidden") }
-	}, [ controls, inView ] )
+	useEffect(() => {
+		if (inView) {
+			controls.start("visible");
+		}
+		if (!inView) {
+			controls.start("hidden");
+		}
+	}, [controls, inView]);
 
 	return (
-		<m.section 	
+		<m.section
 			key={index}
-			className={css.project} 
+			className={css.project}
 			//framer-motion
 			ref={ref}
 			variants={container}
-			initial={[ "rest", "hidden" ]}
-			whileHover="hover"
-			animate={controls} >
-			
+			initial={["rest", "hidden"]}
+			whileHover='hover'
+			animate={controls}>
 			<div className={css.details}>
 				<div className={css.projectHeader}>
-					<div className={css.header}>
-						<h3 className="highlight">{project}</h3><span className={css.privateOr}><i className="devicon-github-plain"></i>{repo}</span>	
+					<div
+						onClick={() => window.open(url, "_blank")}
+						className={`${css.header} leavesite`}>
+						<h3 className='highlight'>{project}</h3>
+						<span className={css.privateOr}>
+							<i className='devicon-github-plain'></i>
+							{repo}
+						</span>
 					</div>
 					<div className={css.description}>
-						<p><strong>{descriptionTitle}</strong> {description}</p>
+						<p>
+							<strong>{descriptionTitle}</strong>{" "}
+						</p>
+					</div>
+					<div className={css.features}>
+						<p>
+							<strong>{features}</strong>{" "}
+						</p>
 					</div>
 					<div className={css.stackContainer}>
-						<Badges list={stack} block="stack" fullContainer={false} color={false} />
+						<Badges
+							list={stack}
+							block='stack'
+							fullContainer={false}
+							color={false}
+						/>
 					</div>
-					<m.div variants={''} className={css.viewProject}>
-						<Icon icon={[ 'fad', 'arrow-right-to-bracket' ]} />
-					</m.div>
+					<button
+						className={`button ${button.primary} leaveSite`}
+						style={{ width: "51%" }}
+						onClick={() => window.open(url, "_blank")}>
+						View Project
+					</button>
 				</div>
 			</div>
 
 			<div className={css.imageContainer}>
-				<span className={`${css.imageAnimationContainer}`}>
-					{ images.map( ({key, url, hover, h, w }, index) => {
-						hover = ( hover === 'left' ) ? hoverLeft : hoverRight
-						return (
-							<m.div key={`${index}-${key}`} variants={item}>
-								<m.div variants={hover}>
-									<Image src={url} alt="x" height={h} width={w} />
-								</m.div>
+				{/* <span className={`${css.imageAnimationContainer}`}> */}
+				{images.map(({ key, url, hover, h, w }, index) => {
+					hover = hover === "left" ? hoverLeft : hoverRight;
+					return (
+						<m.div key={`${index}-${key}`} variants={item}>
+							<m.div variants={hover}>
+								<Image src={url} alt='x' height={h} width={w} />
 							</m.div>
-						)}
-					) }
-				</span>
+						</m.div>
+					);
+				})}
+				{/* </span> */}
 			</div>
 		</m.section>
-	)
+	);
 }
 
 const container = {
-	hidden: { 
+	hidden: {
 		transition: {
 			delayChildren: 0.125,
-			staggerChildren: 0.0625
-		}
+			staggerChildren: 0.0625,
+		},
 	},
 	visible: {
 		transition: {
 			delayChildren: 0.125,
 			staggerChildren: 0.25,
-		}
+		},
 	},
 	rest: {
 		transition: {
 			delayChildren: 0,
 			staggerChildren: 0,
-		}
+		},
 	},
 	hover: {
 		transition: {
 			delayChildren: 0,
 			staggerChildren: 0,
-		}
-	}
-}
+		},
+	},
+};
 
 const item = {
-	hidden: { 
-		y: 75, 
+	hidden: {
+		y: 75,
 		opacity: 0,
 		transition: {
 			type: "tween",
 			ease: "easeIn",
-			duration: .35, 
-		}
+			duration: 0.35,
+		},
 	},
 	visible: {
 		y: 0,
@@ -114,26 +149,25 @@ const item = {
 		transition: {
 			type: "tween",
 			ease: "easeOut",
-			duration: .5, 
-		}
+			duration: 0.5,
+		},
 	},
-}
+};
 
 const hoverLeft = {
 	rest: {
-		x: 0
+		x: 0,
 	},
 	hover: {
-		x: -20
-	}
-}
+		x: -20,
+	},
+};
 
 const hoverRight = {
 	rest: {
-		x: 0
+		x: 0,
 	},
 	hover: {
-		x: 20
-	}
-}
-
+		x: 20,
+	},
+};
